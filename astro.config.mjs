7bd01067067fc8028b02f57ca0840491904a7fff
Jarema <git@jarema.me>
2024-06-27 11:04:45 +0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import SpeedInsights  from "@vercel/speed-insights/astro"
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astrosphere-site.vercel.app",
+  site: "https://jar.tf",
   integrations: [mdx(), sitemap(), solidJs(), icon(), tailwind({ applyBaseStyles: false })],
 })
